e3abeb8458eec9248f9299fcb86fa72a5bba10f1
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-08-21 00:50:34 +0530

    color theme added

---
+++ lib/Theme/Colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+const Color primaryColor = Color(0xFF3498db);
+
+
+Color secondaryColor = Color(0xFFf1c40f);
+
+
+Color accentColor = Color(0xFF2ecc71);
+
+
+Color backgroundColor = Color(0xFFf5f5f5);
