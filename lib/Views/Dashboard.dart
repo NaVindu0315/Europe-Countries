5a0b96601f0af3e391e11d30f9bb6aba22e36d52
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-08-21 00:53:19 +0530

    countriess list displaying need to exapndable widget next

--- lib/Views/Dashboard.dart
+++ lib/Views/Dashboard.dart
@@ -1,4 +1,10 @@
+import 'package:europe_countries/Services/Countries_Fetch.dart';
+import 'package:europe_countries/Services/Test_Function.dart';
+import 'package:europe_countries/Widgets/CountryCard.dart';
 import 'package:flutter/material.dart';
+
+import '../Models/Countries_Model.dart';
+import '../Widgets/CircularProgress.dart';
 
 class Dashboard extends StatefulWidget {
   const Dashboard({Key? key}) : super(key: key);
@@ -18,7 +24,7 @@ class _DashboardState extends State<Dashboard> {
         body: SingleChildScrollView(
           child: Column(
             children: [
-              Row(
+              const Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Text('Test'),
@@ -28,11 +34,51 @@ class _DashboardState extends State<Dashboard> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   ElevatedButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      fetchData_test();
+                    },
                     child: Text('Test'),
                   )
                 ],
               ),
+              SizedBox(
+                height: 800,
+                child: FutureBuilder(
+                    future: fetchCountries(),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        List<Country> countries =
+                            snapshot.data as List<Country>;
+                        return ListView.builder(
+                            itemCount: countries.length,
+                            itemBuilder: (context, index) {
+                              Country country = countries[index];
+                              return CountryCard(
+                                  flagLink: country.flagLink,
+                                  nameCommon: country.nameCommon,
+                                  nameOfficial: country.nameOfficial,
+                                  capital: country.capital,
+                                  region: country.region,
+                                  languageNno: country.languageNno,
+                                  languageNob: country.languageNob,
+                                  languageSmi: country.languageSmi,
+                                  population: country.population);
+                            });
+                      } else if (snapshot.hasError) {
+                        print(snapshot.data);
+                        print(snapshot.error);
+                        return Text(
+                            'Failed Fetch Countries : ${snapshot.error}');
+                      } else {
+                        return //CircularProgressIndicator();
+                            Column(
+                          children: [
+                            Center(child: circularProgress()),
+                          ],
+                        );
+                      }
+                    }),
+              )
             ],
           ),
         ),
