a8b22bf4afe57f13e5686694275df1a517910627
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-08-20 22:59:01 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/navindu/europe_countries/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.navindu.europe_countries
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Views/Dashboard.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatefulWidget {
+  const Dashboard({Key? key}) : super(key: key);
+
+  @override
+  State<Dashboard> createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Text('Test'),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
