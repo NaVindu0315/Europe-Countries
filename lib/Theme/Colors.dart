b59a7a0a13f4e1e94a12483f065ec419a9c947a8
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-08-21 23:06:20 +0530

    color changed

--- lib/Theme/Colors.dart
+++ lib/Theme/Colors.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
 
+// Primary Color (App Bar Color)
+const appBarColor = Color(0xFF3498db);
 
-const Color primaryColor = Color(0xFF3498db);
+// Background Color
+const backgroundColor = Color(0xFFF7F7F7);
 
+// Card Background Color
+const cardBackgroundColor = Color(0xFFFFFFFF);
 
-Color secondaryColor = Color(0xFFf1c40f);
+// Accent Color
+const accentColor = Color(0xFF8bc34a);
 
+// Text Color
+const textColor = Color(0xFF333333);
 
-Color accentColor = Color(0xFF2ecc71);
-
-
-Color backgroundColor = Color(0xFFf5f5f5);
+// Optional Gradient
+Gradient gradient = const LinearGradient(
+  begin: Alignment.topCenter,
+  end: Alignment.bottomCenter,
+  colors: [Color(0xFF3498db), Color(0xFFF7F7F7)],
+);
