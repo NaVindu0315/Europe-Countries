60399c31d51eb04703b2ddf720e4d81bf4cd2af3
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-08-22 10:06:58 +0530

    work with error handling

--- lib/Views/Dashboard.dart
+++ lib/Views/Dashboard.dart
@@ -36,110 +36,107 @@ class _DashboardState extends State<Dashboard> {
                 SizedBox(
                   height: 1400,
                   child: FutureBuilder(
-                      future: fetchCountry2(),
-                      builder: (context, snapshot) {
-                        if (snapshot.hasData) {
-                          List<Country> countries =
-                              snapshot.data as List<Country>;
-                          switch (_sortBy) {
-                            case SortBy.nameCommon:
-                              countries.sort((a, b) =>
-                                  a.nameCommon.compareTo(b.nameCommon));
-                              break;
-                            case SortBy.capital:
-                              countries.sort(
-                                  (a, b) => a.capital.compareTo(b.capital));
-                              break;
-                            case SortBy.population:
-                              countries.sort((a, b) =>
-                                  a.population.compareTo(b.population));
-                              break;
-                          }
-                          return Column(
-                            children: [
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  DropdownButton<SortBy>(
-                                    value: _sortBy,
-                                    onChanged: (value) {
-                                      setState(() {
-                                        _sortBy = value!;
-                                      });
-                                    },
-                                    items: const [
-                                      DropdownMenuItem(
-                                        value: SortBy.nameCommon,
-                                        child: Text('Sort by Name'),
-                                      ),
-                                      DropdownMenuItem(
-                                        value: SortBy.capital,
-                                        child: Text('Sort by Capital'),
-                                      ),
-                                      DropdownMenuItem(
-                                        value: SortBy.population,
-                                        child: Text('Sort by Population'),
-                                      ),
-                                    ],
-                                  ),
-                                ],
-                              ),
-                              SizedBox(
-                                height: 10,
-                              ),
-                              SizedBox(
-                                height: 1000,
-                                child: ListView.builder(
-                                    itemCount: countries.length,
-                                    itemBuilder: (context, index) {
-                                      Country country = countries[index];
-                                      return Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        children: [
-                                          CountryCard(
-                                              flagLink: country.flagLink,
-                                              nameCommon: country.nameCommon,
-                                              nameOfficial:
-                                                  country.nameOfficial,
-                                              capital: country.capital,
-                                              region: country.region,
-                                              languageNno: country.languageNno,
-                                              languageNob: country.languageNob,
-                                              languageSmi: country.languageSmi,
-                                              population: country.population),
-                                        ],
-                                      );
-                                    }),
-                              ),
-                            ],
-                          );
-                        } else if (snapshot.hasError) {
-                          /*  print(snapshot.data);
-                          print(snapshot.error);
-                          return Text(
-                              'Failed Fetch Countries : ${snapshot.error}');*/
-                          if (snapshot.error is SocketException) {
-                            return const Text(
-                                'Please make sure you are connected to the internet.');
-                          } else if (snapshot.error is TimeoutException) {
-                            return const Text(
-                                'Connection timed out. Please try again.');
-                          } else if (snapshot.error is HttpException) {
-                            return const Text(
-                                'Failed to load data. Please try again.');
-                          } else {
-                            return Text('An error occurred: ${snapshot.error}');
-                          }
+                    future: fetchCountry2(),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Center(child: circularProgress()),
+                          ],
+                        );
+                      } else if (snapshot.hasError) {
+                        if (snapshot.error is SocketException) {
+                          return const Text(
+                              'Please make sure you are connected to the internet.');
+                        } else if (snapshot.error is TimeoutException) {
+                          return const Text(
+                              'Connection timed out. Please try again.');
+                        } else if (snapshot.error is HttpException) {
+                          return const Text(
+                              'Failed to load data. Please try again.');
                         } else {
-                          return //CircularProgressIndicator();
-                              Column(
-                            children: [
-                              Center(child: circularProgress()),
-                            ],
-                          );
+                          return const Text(
+                              'Please Make Sure You have an active internet Connection and try again');
                         }
-                      }),
+                      } else {
+                        List<Country> countries =
+                            snapshot.data as List<Country>;
+                        switch (_sortBy) {
+                          case SortBy.nameCommon:
+                            countries.sort(
+                                (a, b) => a.nameCommon.compareTo(b.nameCommon));
+                            break;
+                          case SortBy.capital:
+                            countries
+                                .sort((a, b) => a.capital.compareTo(b.capital));
+                            break;
+                          case SortBy.population:
+                            countries.sort(
+                                (a, b) => a.population.compareTo(b.population));
+                            break;
+                        }
+                        return Column(
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                DropdownButton<SortBy>(
+                                  value: _sortBy,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      _sortBy = value!;
+                                    });
+                                  },
+                                  items: const [
+                                    DropdownMenuItem(
+                                      value: SortBy.nameCommon,
+                                      child: Text('Sort by Name'),
+                                    ),
+                                    DropdownMenuItem(
+                                      value: SortBy.capital,
+                                      child: Text('Sort by Capital'),
+                                    ),
+                                    DropdownMenuItem(
+                                      value: SortBy.population,
+                                      child: Text('Sort by Population'),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            SizedBox(
+                              height: 1000,
+                              child: ListView.builder(
+                                  itemCount: countries.length,
+                                  itemBuilder: (context, index) {
+                                    Country country = countries[index];
+                                    return Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        CountryCard(
+                                            flagLink: country.flagLink,
+                                            nameCommon: country.nameCommon,
+                                            nameOfficial: country.nameOfficial,
+                                            capital: country.capital,
+                                            region: country.region,
+                                            languageNno: country.languageNno,
+                                            languageNob: country.languageNob,
+                                            languageSmi: country.languageSmi,
+                                            population: country.population),
+                                      ],
+                                    );
+                                  }),
+                            ),
+                          ],
+                        );
+                      }
+                    },
+                  ),
                 )
               ],
             ),
