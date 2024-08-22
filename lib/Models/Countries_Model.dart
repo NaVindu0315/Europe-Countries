eba05c11f56268831ab58f9681d963fd78fa6a7e
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-08-22 11:34:55 +0530

    model class changed to fetch native names and languages

--- lib/Models/Countries_Model.dart
+++ lib/Models/Countries_Model.dart
@@ -1,23 +1,22 @@
+//models country,languges ,native names
 class Country {
   String flagLink;
   String nameCommon;
   String nameOfficial;
+  Map<String, NativeName> nativeNames;
   String capital;
   String region;
-  String languageNno;
-  String languageNob;
-  String languageSmi;
+  Map<String, Language> languages;
   int population;
 
   Country({
     required this.flagLink,
     required this.nameCommon,
     required this.nameOfficial,
+    required this.nativeNames,
     required this.capital,
     required this.region,
-    required this.languageNno,
-    required this.languageNob,
-    required this.languageSmi,
+    required this.languages,
     required this.population,
   });
 
@@ -26,12 +25,47 @@ class Country {
       flagLink: json['flags']['png'],
       nameCommon: json['name']['common'],
       nameOfficial: json['name']['official'],
+      nativeNames: (json['name']['nativeName'] as Map<String, dynamic>)
+          .map((key, value) => MapEntry(key, NativeName.fromJson(value))),
       capital: json['capital'][0],
       region: json['region'],
-      languageNno: json['languages']['nno'] ?? '',
-      languageNob: json['languages']['nob'] ?? '',
-      languageSmi: json['languages']['smi'] ?? '',
+      languages: (json['languages'] as Map<String, dynamic>)
+          .map((key, value) => MapEntry(key, Language.fromJson(key, value))),
       population: json['population'],
+    );
+  }
+}
+
+class NativeName {
+  String official;
+  String common;
+
+  NativeName({
+    required this.official,
+    required this.common,
+  });
+
+  factory NativeName.fromJson(Map<String, dynamic> json) {
+    return NativeName(
+      official: json['official'],
+      common: json['common'],
+    );
+  }
+}
+
+class Language {
+  String code;
+  String name;
+
+  Language({
+    required this.code,
+    required this.name,
+  });
+
+  factory Language.fromJson(String code, String name) {
+    return Language(
+      code: code,
+      name: name,
     );
   }
 }
